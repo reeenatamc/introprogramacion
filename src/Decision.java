ce3d60ca059142a32c4549999f7fb7d3178aa9fb
Christopher Jaramillo <108904364+Christopherj132@users.noreply.github.com>
2022-11-01 16:14:41 -0500

    Condicionales

--- src/Decision.java
+++ src/Decision.java
@@ -1,2 +1,19 @@
-package PACKAGE_NAME;public class Decision {
+import java.util.Scanner;
+
+/**
+ * Programa para determinar si una persona es mayor o menor de edades
+ */
+public class Decision {
+    public static void main(String[] args) {
+        System.out.println("Programa para determinar si una persona es mayor de edad");
+        Scanner teclado = new Scanner (System.in);
+        System.out.println("Ingrese su edad: ");
+        int edad = teclado.nextInt();
+        if (edad >= 18) {
+            System.out.println("eres mayor de edad");
+        }else{
+            System.out.println("eres menor de edad");
+            System.out.println("te falta madurar :*");
+        }
+    }
 }
--- src/DiaSemana.java
+++ src/DiaSemana.java
@@ -1,2 +1,28 @@
-package PACKAGE_NAME;public class DiaSemana {
-}
+import java.util.Scanner;
+
+public class DiaSemana {
+    public static void main(String[] args) {
+        System.out.println("Programa para determinar que dia de la semana es");
+        Scanner teclado = new Scanner(System.in);
+        System.out.println("Ingrese el numero: ");
+        int dia1 = teclado.nextInt();
+
+        if (dia1 == 1)
+            System.out.println("Es lunes");
+        else if (dia1 == 2)
+            System.out.println("Es martes");
+        else if (dia1 == 3)
+            System.out.println("Es miercoles");
+        else if (dia1 == 4)
+            System.out.println("Es jueves");
+        else if (dia1 == 5)
+            System.out.println("Es viernes");
+        else if (dia1 == 6)
+            System.out.println("Es sabado");
+        else if (dia1 == 7)
+            System.out.println("Es viernes");
+        else
+            System.out.println("Erroooor");
+
+        }
+    }
